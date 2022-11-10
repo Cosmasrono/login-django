0ce313991b9dce04310b8357e5ad22c5fb12d322
Cosmasrono <ccosmas001@gmail.com>
2022-11-10 21:09:09 +0300

    changes

--- main/models.py
+++ main/models.py
@@ -1,19 +1,5 @@
-from pyexpat import model
-from django.db import models
-from django.contrib.auth.models import User
-import sys
-sys.path.insert(0, './yolov5')
-# Create your models here.
 
-class post():
-    title=models.CharField
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
-    body=models.TextField
-    
-    
-    
-    def __str__(self):
-        return self.title
+# Create your models here.
     
     
     
--- main/views.py
+++ main/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 def home(request):
     
     return render(request, "main.html")
+def register(request):
+    
+   if request.method=='POST':
+       fname=request.POST('fname')
+       sname=request.POST('sname')
+       email=request.POST('email')
+       password1=request.POST('password')
+       password2=request.POST('password2') 
+       user=User.objects.create_user(password=password1,email=email,fname=fname,sname=sname)
+       user.save()
+       print('user created')
+   return render(request,'register.html')
+   
